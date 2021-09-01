f811842fb0af2f4d16d30f0aaa34b1fd7dee1a4a
Temitope Olamolu <olamolutemitope@gmail.com>
2021-09-01 11:55:36 +0100

    fix kernel format

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,16 +9,6 @@
  * Return: largest number
  */
 
-#include "holberton.h"
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
